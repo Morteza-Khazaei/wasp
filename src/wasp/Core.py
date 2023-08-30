de29587cc6b561f4da65acac08df4fe92de736cb
Morteza-Khazaei <mortezakhazaei1370@gmail.com>
2023-08-30 09:32:48 +0200

    debugging

--- src/wasp/Core.py
+++ src/wasp/Core.py
@@ -23,6 +23,7 @@ class WaspHandeler(BaseComparison):
 
 
         self.rep_l2, self.rep_l3, self.wasp = args.input, args.out, args.wasp
+        print(self.rep_l2, self.rep_l3, self.wasp)
         self.l2_products = self.__get_all_available_products()
         self.fl2_products = self.__filter_products()
 
