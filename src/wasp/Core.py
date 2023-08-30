c8d585e65e24a7fbed4d2583e294651d01065a6f
Morteza-Khazaei <mortezakhazaei1370@gmail.com>
2023-08-30 10:06:22 +0200

    Resolved bugs

--- src/wasp/Core.py
+++ src/wasp/Core.py
@@ -49,7 +49,7 @@ class WaspHandeler(BaseComparison):
                         # Group by same month
                         group_by_date[dt_jalali].append(os.path.join(root, name))
             if any(dict(group_by_date)):
-                l2[tile].append(group_by_date)
+                l2[tile].append(dict(group_by_date))
 
         return l2
 
